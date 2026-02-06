6058581236fc6140303dc75defcf9b69fa9874d7
musharaf1912 <musharaf@lifeboat.co.in>
2026-02-06 13:11:34 +0530

    Event Cell Updated

--- FAMILYFIRST/Family/MonthCell.swift
+++ FAMILYFIRST/Family/MonthCell.swift
@@ -12,17 +12,45 @@ class MonthCell: UITableViewCell {
     @IBOutlet weak var dateLbl: UILabel!
     @IBOutlet weak var imgVw: UIImageView!
     @IBOutlet weak var dayLbl: UILabel!
+    @IBOutlet weak var leftVw: UIView!
+    @IBOutlet weak var moreBtn: UIButton!
     @IBOutlet weak var eventLbl: UILabel!
     @IBOutlet weak var mainVw: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        mainVw.layer.cornerRadius = 8
+        mainVw.clipsToBounds = true
     }
     
     func configure(with event: Event) {
         dateLbl.text = event.dateFormatted
         dayLbl.text = event.daysToGo
         eventLbl.text = event.eventName
+        
+        if let colorHex = event.colourCode, !colorHex.isEmpty {
+            let color = UIColor(hexString: colorHex) ?? UIColor(hexString: "#076839") ?? .systemGreen
+            
+            leftVw.backgroundColor = color
+            
+            mainVw.layer.borderWidth = 2
+            mainVw.layer.borderColor = color.cgColor
+            
+            moreBtn.tintColor = color
+            moreBtn.setTitleColor(color, for: .normal)
+            
+            if let image = moreBtn.imageView?.image {
+                moreBtn.setImage(image.withRenderingMode(.alwaysTemplate), for: .normal)
+            }
+        } else {
+            let defaultColor = UIColor(hexString: "#076839") ?? .systemGreen
+            leftVw.backgroundColor = defaultColor
+            mainVw.layer.borderWidth = 2
+            mainVw.layer.borderColor = defaultColor.cgColor
+            moreBtn.tintColor = defaultColor
+            moreBtn.setTitleColor(defaultColor, for: .normal)
+        }
         
         if let eventInfo = event.eventInfo?.first,
            let imageUrlString = eventInfo.profileImage,
